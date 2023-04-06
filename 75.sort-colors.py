af76b713f2ceff9992fd4d05123c6a2cdf364bf1
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-07 01:06:36 +0300

    solve number 75

---
+++ 75.sort-colors.py
@@ -0,0 +1,22 @@
+from typing import List
+#
+# @lc app=leetcode id=75 lang=python3
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[j] < nums[i]:
+                    nums[j],nums[i] = nums[i],nums[j]
+
+        
+# @lc code=end
+
+
