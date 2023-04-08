7a61f5936ec7c02db5acaf0816fa728b43676087
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-08 08:07:35 +0300

    add description for 1

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -9,9 +9,16 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        # @lc code=end
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if i!=j and nums[i] + nums[j] == target:
                     return [i,j]
+# @lc code=end
+
+# The steps I took to solve this problem:
+# 1. I set up a nested for loop that runs through the elements of nums list.
+# 2. It returns the first occurence where the elements add up to a target.
+# 3. It ignores the case where both the elements to add are equal.
+
+
         
